574984a6ba08556c5046f50a23cda8e39f6cc99c
ericlau00 <ericlau41802@gmail.com>
2020-08-15 19:07:02 -0400

    Create DOM elements via JS

--- main.js
+++ main.js
@@ -1,6 +1,78 @@
 let $ = (id) => document.getElementById(id);
 
+let create = (element_type) => document.createElement(element_type);
+
+let createCheckDiv = (item_id) => {
+    let todoCheck = create('input');
+    todoCheck.type = 'checkbox';
+
+    todoCheck.addEventListener('change', () => {
+        console.log(item_id, 'complete toggle');
+        // update the state of the item in the database
+        // indicate that the todo item has been completed
+    });
+
+    return todoCheck;
+}
+
+let createDateDiv = (date) => {
+    let smallContainer = create('div');
+
+    let todoDate = create('small');
+    todoDate.textContent = `Created on ${date}`; // TODO: format the date better
+
+    smallContainer.appendChild(todoDate);
+
+    return smallContainer;
+}
+
+let createContentDiv = (label, date) => {
+    let todoContent = create('div');
+    todoContent.className = 'item-content';
+
+    let todoLabel = create('label');
+    todoLabel.textContent = label;
+
+    let dateDiv = createDateDiv(date);
+
+    todoContent.appendChild(todoLabel);
+    todoContent.appendChild(dateDiv);
+
+    return todoContent;
+}
+
+let createDeleteDiv = (item_id) => {
+    let todoDelete = create('div');
+    todoDelete.className = 'delete';
+    todoDelete.textContent = 'Delete';
+
+    todoDelete.addEventListener('click', () => {
+        console.log(item_id, 'delete item');
+        // remove item from the database
+        // remove the parent element
+    });
+
+    return todoDelete;
+}
+
+let createTodoItem = (item_id, label, date) => {
+    let todoItem = create('div');
+    todoItem.className = 'list-item';
+
+    let todoCheck = createCheckDiv(item_id);
+    let todoContent = createContentDiv(label, date);
+    let todoDelete = createDeleteDiv(item_id);
+
+    todoItem.appendChild(todoCheck);
+    todoItem.appendChild(todoContent);
+    todoItem.appendChild(todoDelete);
+
+    return todoItem;
+}
+
 window.onload = () => {
+    let container = $('list-items');
+
     $('submit-todo').addEventListener('click', (e) => {
         e.preventDefault();
 
@@ -9,10 +81,13 @@ window.onload = () => {
         let content = todo_input.value.trim();
         if (content.length > 0) {
             let xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
+            xhttp.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 201) {
-                    let todo = JSON.parse(this.responseText);
-                    console.log(todo);
+                    let { item_id, label, date } = JSON.parse(this.responseText);
+
+                    let todoItem = createTodoItem(item_id, label, date);
+
+                    container.appendChild(todoItem);
                 }
             }
             xhttp.open('POST', 'add_todo.php', true);
