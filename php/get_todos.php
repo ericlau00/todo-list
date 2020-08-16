dcd7514eb998d036712edc193869469c9ed9fde3
ericlau00 <ericlau41802@gmail.com>
2020-08-16 12:57:56 -0400

    Add server side sorting functionality

--- php/get_todos.php
+++ php/get_todos.php
@@ -1,15 +1,27 @@
 <?php
 require_once('db_connect.php');
 
-http_response_code(200);
+$method = $_SERVER['REQUEST_METHOD'];
 
-$res = $db->query('SELECT * FROM todo_list');
+if ($method == 'GET') {
+    http_response_code(200);
 
-$data = array();
+    $split = (array_key_exists('sort_by', $_GET))
+        ? explode('.', $_GET['sort_by'])
+        : array('id', 'ASC');
 
-while ($item = $res->fetchArray(SQLITE3_ASSOC)) {
-    array_push($data, $item);
+    $res = $db->query("SELECT * FROM todo_list ORDER BY $split[0] $split[1]");
+
+    $data = array();
+
+    if (gettype($res) != 'boolean') {
+        while ($item = $res->fetchArray(SQLITE3_ASSOC)) {
+            array_push($data, $item);
+        }
+    }
+
+    $db->close();
+
+    echo json_encode($data);
 }
-
-echo json_encode($data);
 exit;
